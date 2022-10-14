289dcddeda305ff7a6a5da627a3b4bf5e3536532
Bui Gia An <buigiaanfb1@gmail.com>
2022-10-14 22:31:05 +0700

    [FIX] Show HTML as a image instead of raw HTML

--- client/src/components/atoms/Modal/Modal.tsx
+++ client/src/components/atoms/Modal/Modal.tsx
@@ -12,7 +12,7 @@ const Modal: React.FC<ModalProps> = ({
   hide,
   content,
 }: ModalProps) =>
-  isShowing
+  isShowing && content
     ? ReactDOM.createPortal(
         <React.Fragment>
           <div
@@ -27,7 +27,7 @@ const Modal: React.FC<ModalProps> = ({
             tabIndex={-1}
             role="dialog"
           >
-            <code>{content}</code>
+            <div dangerouslySetInnerHTML={{ __html: content }}></div>
           </div>
         </React.Fragment>,
         document.body
